01f460edd4efafee1d20fd0fd4dc09fd4d62a08d
Rasel Hossain <codewithruss@gmail.com>
2023-12-22 03:23:29 +0600

    responsive for mobile and desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,53 +9,98 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(true);
   return (
     <header className=" bg-dark_1 shadow">
-      <div className="container mx-auto px-3 flex justify-between items-center h-14">
+      <div className="container mx-auto p-3 flex justify-between items-center ">
         <div>
           <img src={logo} alt="logo" />
         </div>
-        <div className="flex rounded-full border-orange_primary px-1 w-36 items-center  border-2">
-          <form>
-            <input
-              className=" w-full text-xs bg-transparent outline-none text-white p-1"
-              type="text"
-              name="search"
-              id="search"
-            />
-          </form>
+        {/* with hamburg */}
+        <div
+          className={`top-0 w-9/12 md:hidden  bottom-0 p-10 ${
+            toggle ? "hidden" : "block"
+          } absolute bg-dark_1 left-0`}
+        >
+          <ul className=" flex flex-col  text-dark_7 gap-5 text-lg">
+            <Link to={"/"}>Home</Link>
+            <Link to={"/Menu"}>Menu</Link>
+            <Link to={"/Reservation"}>Reservation</Link>
+            <Link to={"/Service"}>Service</Link>
+            <Link to={"/Contact"}>Contact</Link>
+            <div className="flex sm:w-3/4 rounded-full   focus-within:ring-2 ring-orange_primary border-orange_shade_4 px-3  items-center justify-between  border">
+              <form className=" w-full ">
+                <input
+                  className="py-2 px-1 w-full text-sm  bg-transparent outline-none text-white "
+                  type="text"
+                  name="search"
+                  id="search"
+                />
+              </form>
+              <span>
+                <img className=" w-5" src={search_logo} alt="logo" />
+              </span>
+            </div>
+            <Link
+              className=" bg-orange_primary font-medium active:ring ring-orange-300 text-center py-3 rounded-full sm:w-3/4"
+              to={"/Login"}
+            >
+              Login
+            </Link>
+          </ul>
+        </div>
+        {/* without hamburg */}
+        <div className=" hidden md:block  ">
+          <ul className=" items-center text-dark_7 flex gap-x-5  text-lg">
+            <Link to={"/"}>Home</Link>
+            <Link to={"/Menu"}>Menu</Link>
+            <Link to={"/Reservation"}>Reservation</Link>
+            <Link to={"/Service"}>Service</Link>
+            <Link to={"/Contact"}>Contact</Link>
+            <div className=" rounded-full  border-orange_primary px-3 hidden lg:inline-flex items-center justify-between  border-2">
+              <form className=" w-full ">
+                <input
+                  className="py-2 px-2 w-full text-sm sm:text-base bg-transparent  outline-none text-white "
+                  type="text"
+                  name="search"
+                  id="search"
+                />
+              </form>
+              <span>
+                <img className=" w-5" src={search_logo} alt="logo" />
+              </span>
+            </div>
+          </ul>
+        </div>
+        <div className=" flex text-dark_7  items-center gap-2">
           <span>
-            <img className=" w-5" src={search_logo} alt="logo" />
+            <Link to={"/cart"} className="flex">
+              <img src={cart_logo} />
+              <span className="w-4 -ml-2 -mt-2 text-xs h-4 text-orange_primary font-bold bg-white flex justify-center items-center rounded-full">
+                {2}
+              </span>
+            </Link>
+          </span>
+
+          <span>
+            <Link
+              className=" hidden text-lg font-medium md:block bg-orange_primary active:ring ring-orange-300 text-center px-10 py-2 md:py-3  rounded-full"
+              to={"/Login"}
+            >
+              Login
+            </Link>
+          </span>
+          {/* hamburg section  */}
+          <span>
+            <button
+              className=" block  md:hidden"
+              onClick={() => setToggle(!toggle)}
+            >
+              {toggle ? (
+                <img src={hamburg_logo} />
+              ) : (
+                <img src={close_hamburg} />
+              )}
+            </button>
           </span>
         </div>
-        <div className=" flex  gap-2">
-          <Link to={"/cart"} className="flex">
-            <img src={cart_logo} />
-            <span className="w-4 -ml-2 -mt-2 text-xs h-4 text-orange_primary font-bold bg-white flex justify-center items-center rounded-full">
-              {2}
-            </span>
-          </Link>
-          <button onClick={() => console.log(setToggle(!toggle))}>
-            {toggle ? <img src={hamburg_logo} /> : <img src={close_hamburg} />}
-          </button>
-        </div>
-      </div>
-      <div
-        className={`top-0 text-dark_7 w-9/12  bottom-0 p-10 ${
-          toggle ? "hidden" : "block"
-        } absolute bg-dark_1 left-0`}
-      >
-        <ul className=" flex flex-col gap-5 text-lg">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/Menu"}>Menu</Link>
-          <Link to={"/Reservation"}>Reservation</Link>
-          <Link to={"/Service"}>Service</Link>
-          <Link to={"/Contact"}>Contact</Link>
-          <Link
-            className=" bg-orange_primary active:ring ring-orange-300 text-center py-3 rounded-full font-bold"
-            to={"/Login"}
-          >
-            Login
-          </Link>
-        </ul>
       </div>
     </header>
   );
