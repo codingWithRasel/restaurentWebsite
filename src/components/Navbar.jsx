d2edfbeb675539b0b1b703285c4504bb0b81520d
Rasel Hossain <codewithruss@gmail.com>
2023-12-21 22:16:25 +0600

    mobile navber done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../assets/image/logo.png";
+import search_logo from "../assets/image/search.png";
+import cart_logo from "../assets/image/cart_logo.png";
+import hamburg_logo from "../assets/image/hamburg_logo.png";
+const Navbar = () => {
+  return (
+    <header className=" bg-dark_1 shadow ">
+      <div className=" container mx-auto px-5 flex justify-between items-center h-12">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="flex rounded-full border-orange_primary px-1 w-32 items-center  border-2">
+          <input
+            className=" w-full bg-transparent"
+            type="text"
+            name="search"
+            id="search"
+          />
+          <span>
+            <img src={search_logo} alt="logo" />
+          </span>
+        </div>
+        <div className=" flex gap-2">
+          <span>
+            <img src={cart_logo} />
+          </span>
+          <span>
+            <img src={hamburg_logo} />
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      desk: "375px",
+    },
+    extend: {
+      colors: {
+        orange_primary: " #F16214",
+        orange_shade_1: " #F48143",
+        orange_shade_2: " #F48143",
+        orange_shade_3: " #F48143",
+        orange_shade_4: " #F48143",
+        orange_shade_5: " #F48143",
+        orange_shade_6: " #F48143",
+        dark_1: "#0D0C0C",
+        dark_2: "#3D3D3D",
+        dark_3: "#6E6D6D",
+        dark_4: "#9E9E9E",
+        dark_5: "#CFCECE",
+        dark_6: "#E7E7E7",
+        dark_7: "#E7E7E7",
+      },
+      fontFamily: {
+        robot: "'Roboto', sans-serif",
+        imperial: "'Imperial Script', cursive",
+        mont: "Montserrat', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
