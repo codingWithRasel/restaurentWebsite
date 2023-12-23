a1b362acf54d38ef317581b08ccb9a26625d5280
Rasel Hossain <codewithruss@gmail.com>
2023-12-24 00:31:03 +0600

    edit banner for every page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = ({ h2, h3 }) => {
   return (
-    <div className=" bg-banner flex flex-col items-center gap-y-5  bg-cover bg-center py-10 md:p-20">
+    <div className=" bg-banner flex flex-col items-center gap-y-1 bg-cover  bg-center py-10 md:gap-y-5 md:p-20">
       <h2 className=" text-2xl font-bold text-white md:text-5xl">{h2}</h2>
       <h3 className=" text-lg md:text-5xl">{h3}</h3>
     </div>
